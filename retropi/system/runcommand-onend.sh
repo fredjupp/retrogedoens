23eabbedbd3054122a467d403fa258a69efcdaf7
Fred Jupp Kowalski <fredjupp@gmx.de>
2018-02-25 11:21:29 +0100

    added system scripts

---
+++ retropi/system/runcommand-onend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#  Useful data are passed as arguments to these scripts:
+#   from https://retropie.org.uk/docs/Runcommand/
+#      $1 - the system (eg: atari2600, nes, snes, megadrive, fba, etc).
+#      $2 - the emulator (eg: lr-stella, lr-fceumm, lr-picodrive, pifba, etc).
+#      $3 - the full path to the rom file.
+#      $4 - the full command line used to launch the emulator.
+# install to: /opt/retropie/configs/all
+# output info about game on LED
+echo "End of  - $1 : $(basename $3) - Emulator: $2" | logger -t MYLED
