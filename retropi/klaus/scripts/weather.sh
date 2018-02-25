4b28721dd9ecda0b1bfccd8dfb5bda5f5faae067
Fred Jupp Kowalski <fredjupp@gmx.de>
2018-02-25 11:04:05 +0100

    suppressed duplicate headline and multiply sleep time by 5

--- retropi/klaus/scripts/weather.sh
+++ retropi/klaus/scripts/weather.sh
@@ -3,12 +3,13 @@
 PATH=/home/pi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/home/pi/bin
 export PATH
 sleeptime=${1:-300}
+sleeptime=$((sleeptime *5))
 while true
 do
  # outcp437="$(python /home/pi/klaus/weather.py | iconv -c -f utf-8  -t CP437 )"
 #  python /home/pi/klaus/weather.py | iconv -c -f utf-8 -t CP437
 curl -s http://open.live.bbc.co.uk/weather/feeds/en/2934691/3dayforecast.rss | 
-awk -F"<|>" '/title/{print $3}' | iconv -c -f utf-8  -t CP437
+awk -F"<|>" '$1=="      " && /title/{print $3}' | iconv -c -f utf-8  -t CP437
 #  printf "${outcp437}"
   sleep $sleeptime
 done
