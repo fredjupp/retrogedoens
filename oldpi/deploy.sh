4ea1d6a8d258a136bf3320417de668fee6b0e705
Fred Jupp Kowalski <fredjupp@gmx.de>
2018-02-26 09:33:18 +0100

    fixed deploy issues

--- oldpi/deploy.sh
+++ oldpi/deploy.sh
@@ -9,7 +9,7 @@ rsync -av ./klaus/*                       ${hdir}/klaus/
 rsync -av ./bin/killall.sh                ${hdir}/bin/
 rsync -av ./bin/runall.sh                 ${hdir}/bin/
 # adjust permissions
-chmod  755 ${hdir}/bin/* ${hdir}/klaus/* /opt/retropie/configs/all/runcommand-on*.sh
+chmod  755 ${hdir}/bin/* ${hdir}/klaus/* 
 sudo chown root:root /etc/systemd/system/myled.service  /etc/rsyslog.d/myled.conf
 sudo chmod  755 /etc/rsyslog.d /etc/systemd/system/myled.service
 sudo chmod  755 /etc/rsyslog.d
